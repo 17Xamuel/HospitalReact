a37be9d7a261028fd3f1a3f59310cd09108bf9a1
MulindwaDenis13 <mulindwadenis13@gmail.com>
2021-06-11 11:12:37 +0300

    change

--- server/api/doctor.js
+++ server/api/doctor.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
 const conn = require("../database/db");
 
+router.post("/new_clinical_info", async (req, res) => {
+  console.log(req.body);
+});
+
 module.exports = router;
