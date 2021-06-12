b05798eb76939d2412e7d2c5147247a6f69ed663
MulindwaDenis13 <mulindwadenis13@gmail.com>
2021-06-12 11:46:20 +0300

    change

--- server/api/admin.js
+++ server/api/admin.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const conn = require("../database/db");
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   conn.query(
     `SELECT * FROM system_users WHERE user_name=? AND password=?`,
     [req.body.username, req.body.password],
